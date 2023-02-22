ac00771d4eb7d21e917a873467a825145d117603
Andrea Leoncini <andrea.leoncini@gmail.com>
2023-02-22 16:45:09 +0100

    rimossa html table dalle pagine della applicazione web

--- web/src/main/resources/META-INF/resources/js/dashboard.js
+++ web/src/main/resources/META-INF/resources/js/dashboard.js
@@ -2,7 +2,6 @@ var STORE_ORIGIN = window.location.origin;
 
 function subscribe(cf, callback) {
     var theUrl = STORE_ORIGIN + '/subscribe/' + cf;
-    console.log('=====> ' + theUrl);
     $.ajax({
         url: theUrl,
         type: 'POST',
@@ -10,6 +9,19 @@ function subscribe(cf, callback) {
         complete: function(response, status, xhr){
             var iscrizione = jQuery.parseJSON(response.responseText);
             callback(iscrizione);
+        }
+    });
+};
+
+function subscribeReactive(cf, callback) {
+    var theUrl = STORE_ORIGIN + '/request/' + cf;
+    $.ajax({
+        url: theUrl,
+        type: 'POST',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            var cf = response.responseText;
+            callback(cf);
         }
     });
 };
@@ -35,6 +47,17 @@ function showSubscribeResult(iscrizione) {
     }
 };
 
+function showReactiveSubscribeResult(cf) {
+    localStorage.setItem("codiceFiscale", cf);
+    var content = 'Richiesta correttamente inoltrata';
+    $("#save_ok").html(content);
+    $("#save_ok").fadeIn();
+    window.setTimeout(function () { 
+        $("#save_ok").fadeOut();
+        $("#button_save_cf").prop('disabled', false);
+    }, 4000);
+};
+
 function checkActiveRequests() {
     var cf = localStorage.getItem("codiceFiscale");
     var theUrl = STORE_ORIGIN + '/check/' + cf;
@@ -44,27 +67,30 @@ function checkActiveRequests() {
         dataType: 'json',
         complete: function(response, status, xhr){
             var list = jQuery.parseJSON(response.responseText);
-            displayRequestTable(list);
+            displaySubscriptions(list);
         }
     });
     setTimeout(checkActiveRequests, 5000);
 }
 
-function displayRequestTable(list) {
-    $('#requestTableContainer').hide();
-    $("#reqsTableBody").empty();
-    $.each(list, function (index, iscrizione) {
-        addElementToTable(iscrizione);
+function displaySubscriptions(list) {
+    $.each(list, function (index, subscription) {
+        displaySubscription(subscription);
     });
-    $('#requestTableContainer').show(500);
 };
 
-function addElementToTable(iscrizione) {
-    var rowContent = '<tr>';
-    rowContent += '<td>' + iscrizione.codiceProtocollo + '</td>';
-    rowContent += '<td>' + iscrizione.codiceFiscale + '</td>';
-    rowContent += '<td>' + iscrizione.premio + '</td>';
-    rowContent += '<td>' + iscrizione.tipo + '</td>';
-    rowContent += '</tr>';
-    $('#tbl_reqs  tbody').append(rowContent);
+function displaySubscription(iscrizione) {
+    var current;
+    if(iscrizione.tipo == 'imperative'){
+        current = $('#codiceProtocolloClassic').text();
+        if(iscrizione.codiceProtocollo != current){
+            $('#codiceProtocolloClassic').text(iscrizione.codiceProtocollo);
+        }
+    }
+    if(iscrizione.tipo == 'reactive'){
+        current = $('#codiceProtocolloReactive').text();
+        if(iscrizione.codiceProtocollo != current){
+            $('#codiceProtocolloReactive').text(iscrizione.codiceProtocollo);
+        }
+    }
 };
